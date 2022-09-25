da074ae22d888d3fce1fcac47ba906c50f54639f
Denis Milkevich <denmilke987@ukr.net>
2022-09-26 00:48:41 +0200

    checkFirstSaturday

--- FrontEndBasicTasks/1stJanuaryIsSunday/script.js
+++ FrontEndBasicTasks/1stJanuaryIsSunday/script.js
@@ -1,4 +1,4 @@
-const checkFirstSunday = (startYear, endYear) => {
+const checkFirstSaturday = (startYear, endYear) => {
     let time = new Date();
     time.setMonth(0);
     time.setDate(1);
@@ -6,11 +6,11 @@ const checkFirstSunday = (startYear, endYear) => {
     for (let year = startYear; year <= endYear; year++) {
         time.setFullYear(year);
         if (time.getDay() === 6) {
-            console.log(`Yeah, 1st Jan ${year} is Saturday`)
+            console.log(`Yeah, 1st Jan ${year} is Saturday`);
         } else {
             console.log(`Nope, 1st Jan ${year} isn't Saturday`);
         }
     }
 }
 
-checkFirstSunday(2014, 2050);
+checkFirstSaturday(2014, 2050);
