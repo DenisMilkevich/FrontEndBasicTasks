c3562af27f57ff8cca92af05f1e68d8dbd2fe2e7
Denis Milkevich <denmilke987@ukr.net>
2022-09-25 23:31:29 +0200

    Date and Time

--- FrontEndBasicTasks/DateAndTime/script.js
+++ FrontEndBasicTasks/DateAndTime/script.js
@@ -1,8 +1,8 @@
 const getTime = () => {
     const now = new Date();
     const dayslist = ["Sunday","Monday","Tuesday","Wednesday ","Thursday","Friday","Saturday"];
-    return `Current time is : ${now.getHours()}:${now.getMinutes()}:${now.getSeconds()} \nToday is : ${dayslist[now.getDay()]}`;
 
+    return `Current time is : ${now.getHours()}:${now.getMinutes()}:${now.getSeconds()} \nToday is : ${dayslist[now.getDay()]}`;
 }
 
 console.log(getTime());
